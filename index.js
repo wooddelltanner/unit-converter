0305fe8707c2420155123c2f4db3a3c3da7c227c
wooddelltanner <wooddelltanner@gmail.com>
2026-09-13 19:54:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ convertBtn.addEventListener("click", function() {
     const num = Math.floor(numberInput.value * 1000) / 1000
     const feet = (num * 3.281).toFixed(3)
     const meter = (num / 3.281).toFixed(3)
-    const gallon = (num * 0.264).toFixed(3)
-    const liter = (num / 0.264).toFixed(3)
-    const pound = (num * 2.204).toFixed(3)
-    const kilogram = (num / 2.204).toFixed(3)
+    const gallon = (num / 0.264).toFixed(3)
+    const liter = (num * 0.264).toFixed(3)
+    const pound = (num / 2.204).toFixed(3)
+    const kilogram = (num * 2.204).toFixed(3)
     meterFeet.textContent = `${num} meters = ${feet} feet | ${num} feet = ${meter} meters`
     literGallon.textContent = `${num} liters = ${gallon} gallons | ${num} gallons = ${liter} liters`
     kilogramPound.textContent = `${num} kilograms = ${pound} pounds | ${num} pounds = ${kilogram} kilograms`
